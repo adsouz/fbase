a8afbf3dc37a2014ef7bd79685b7d4653c55b041
unknown <adsouz@BLRADSOUZ24890.sapient.com>
2015-10-27 15:37:42 +0530

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testProjApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the testProjApp
+ */
+angular.module('testProjApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
